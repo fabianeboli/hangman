7f779391c9bc78561ac8c23ddcf774035c864b33
Fabian <fabiane@disroot.org>
2019-07-21 11:24:57 +0200

    Refactored, stylized components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Keyboard } from './containers/Keyboard/Keyboard';
-import { Header } from './components/Header/Header';
 import { Hangman } from './containers/Hangman/Hangman';
 
 const App: React.FC = () => {
